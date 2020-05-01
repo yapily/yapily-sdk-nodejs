bf535dbf617936c9268e1b37c927803d14607186
yapily-mofe <57627291+yapily-mofe@users.noreply.github.com>
2020-05-01 16:14:14 +0100

    Remove dummy credentials from constants

--- examples/constants.js
+++ examples/constants.js
@@ -1,7 +1,7 @@
 module.exports = {
-    "APPLICATION_ID" : "54716dbc-36af-472a-b3ad-e6356c75c88d",
-    "APPLICATION_SECRET" : "e5738fc6-fe3f-4634-b00b-13a263643b10",
+    "APPLICATION_ID" : "",
+    "APPLICATION_SECRET" : "",
     "APPLICATION_USER_ID": "node-sdk@yapily.com",
-    "INSTITUTION_ID": "modelo-sandbox",
+    "INSTITUTION_ID": "",
     "CALLBACK_URL" : ""
 }
