d041a5407430abcb9eadbc22dbbf28960a5418c3
yapily-mofe <57627291+yapily-mofe@users.noreply.github.com>
2020-05-01 16:09:48 +0100

    Updated user reference to use applicationUserId

--- examples/constants.js
+++ examples/constants.js
@@ -1,7 +1,7 @@
 module.exports = {
     "APPLICATION_ID" : "54716dbc-36af-472a-b3ad-e6356c75c88d",
     "APPLICATION_SECRET" : "e5738fc6-fe3f-4634-b00b-13a263643b10",
-    "USER_ID": "75fc0bd8-9989-48c6-ba01-157347d8ed54",
+    "APPLICATION_USER_ID": "node-sdk@yapily.com",
     "INSTITUTION_ID": "modelo-sandbox",
     "CALLBACK_URL" : ""
 }
